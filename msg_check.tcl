cb932b3b05a90952924a50f8da1c06d7b35f6856
zsiki <siki@agt.bme.hu>
2017-12-19 09:54:49 +0100

    check for message files revised

--- msg_check.tcl
+++ msg_check.tcl
@@ -1,39 +1,29 @@
 #!/bin/sh
 # the next line restarts using wish \
-exec prowish83 "$0" "$@"
-# GeoEasy angol, nemet es magyar uzenetfile osszehasonlitasa
-source geo_easy.hun
-set f [open hun.txt w]
-foreach i [lsort [array names geoEasyMsg]] { puts $f $i }
-close $f
-unset geoEasyMsg
-source geo_easy.eng
-set f [open eng.txt w]
-foreach i [lsort [array names geoEasyMsg]] { puts $f $i }
-close $f
-catch "exec diff hun.txt eng.txt > hun_eng.txt"
-unset geoEasyMsg
-source geo_easy.ger
-set f [open ger.txt w]
-foreach i [lsort [array names geoEasyMsg]] { puts $f $i }
-close $f
-catch "exec diff hun.txt ger.txt > hun_ger.txt"
-# ComEasy angol, nemet es magyar uzenetfile osszehasonlitasa
-source com_easy.hun
-set f [open hun.txt w]
-foreach i [lsort [array names comEasyMsg]] { puts $f $i }
-close $f
-unset comEasyMsg
-source com_easy.eng
-set f [open eng.txt w]
-foreach i [lsort [array names comEasyMsg]] { puts $f $i }
-close $f
-catch "exec diff hun.txt eng.txt > chun_eng.txt"
-unset comEasyMsg
-source com_easy.ger
-set f [open ger.txt w]
-foreach i [lsort [array names comEasyMsg]] { puts $f $i }
-close $f
-catch "exec diff hun.txt ger.txt > chun_ger.txt"
-file delete hun.txt eng.txt ger.txt
-exit
+exec tclsh "$0" "$@"
+# check language dependent message files of GeoEasy and ComEasy
+set base eng				;# language to compare others to
+set langs [list $base hun]	;# add here more languages after transation
+foreach lang $langs {
+	source geo_easy.$lang
+	set f [open ${lang}.txt w]
+	foreach i [lsort [array names geoEasyMsg]] { puts $f $i }
+	close $f
+	unset geoEasyMsg
+	if {$base != $lang} {
+		catch "exec diff ${base}.txt ${lang}.txt > ${base}_${lang}.txt"
+	}
+}
+foreach lang $langs {
+	source com_easy.$lang
+	set f [open ${lang}.txt w]
+	foreach i [lsort [array names comEasyMsg]] { puts $f $i }
+	close $f
+	unset comEasyMsg
+	if {$base != $lang} {
+		catch "exec diff ${base}.txt ${lang}.txt > c${base}_${lang}.txt"
+	}
+}
+foreach lang $langs {
+	file delete ${lang}.txt
+}
