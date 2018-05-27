8bc43b485b77a358d8a5dd7d8441ffcca186781f
zsiki <siki@agt.bme.hu>
2018-05-27 14:30:43 +0200

    fixes #26 some regression types removed

--- src/geo_easy.tcl
+++ src/geo_easy.tcl
@@ -432,7 +432,7 @@ proc GeoEasy {top} {
 
         menu $topw.menu.regression -tearoff 0
         set i 0
-		set menuBreak {3 4 8}
+		set menuBreak {2}
         foreach r $reglist {
             $topw.menu.regression add command -label $r -command "GeoReg $i"
 			if {[lsearch $menuBreak $i] >= 0} {
