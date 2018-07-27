c291cacdbccd8bd778402ed35267b6e902db9e91
zsiki <siki@agt.bme.hu>
2018-07-27 19:25:12 +0200

    commented out code removed

--- src/index.tcl
+++ src/index.tcl
@@ -2,9 +2,4 @@
 # the next line restarts using tcl \
 exec tclsh "$0" "$@"
 global argv
-#set names {adjgeo.tcl animate.tcl calcgeo.tcl dxfgeo.tcl sdr.tcl \
-#		geodimet.tcl graphgeo.tcl helpgeo.tcl lbgeo.tcl leica.tcl \
-#		loadgeo.tcl maskgeo.tcl profigeo.tcl soc.tcl sokia.tcl travgeo.tcl \
-#		transgeo.tcl printgeo.tcl sentinel.tcl \
-#		topcon.tcl nikon.tcl trimble.tcl}
 	eval auto_mkindex . $argv
