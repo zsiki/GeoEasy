9d17008a0dcb114480088f3871d485af4bf81060
zsiki <siki@agt.bme.hu>
2018-08-24 23:26:53 +0200

    source corrected for tcldoc

--- src/gamaxml.tcl
+++ src/gamaxml.tcl
@@ -17,46 +17,46 @@
 #	Convert XML to list
 #	@param xml input xml data
 proc xml2list xml {
-# skip <? xml ... ?> and <!DOCTYPE >
+	# skip <? xml ... ?> and <!DOCTYPE >
 	regsub {^[ \n\r\t]*<\?xml .*\?>[ \t\n\r]*<!DOCTYPE +gama-.* +SYSTEM \"gama-.*\.dtd\">} $xml "" xml
-# remove everything before <gama-local-adjustment ...
-regsub {^.*<gama-local-adjustment } $xml "<gama-local-adjustment " xml
-# remove all html like comment start and end tag <!-- ... -->
+	# remove everything before <gama-local-adjustment ...
+	regsub {^.*<gama-local-adjustment } $xml "<gama-local-adjustment " xml
+	# remove all html like comment start and end tag <!-- ... -->
 	regsub -all -- {<!--} $xml "" xml
 	regsub -all -- {-->} $xml "" xml
 
-     regsub -all {>\s*<} [string trim $xml " \n\t<>"] "\} \{" xml
-     set xml [string map {> "\} \{#text \{" < "\}\} \{"}  $xml]
-     set res ""   ;# string to collect the result
-     set stack {} ;# track open tags
-     set rest {}
-     foreach item "{$xml}" {
-         switch -regexp -- $item {
-            ^# {append res "{[lrange $item 0 end]} " ; #text item}
-            ^/ {
-                regexp {/(.+)} $item -> tagname ;# end tag
+	regsub -all {>\s*<} [string trim $xml " \n\t<>"] "\} \{" xml
+	set xml [string map {> "\} \{#text \{" < "\}\} \{"}  $xml]
+	set res ""   ;# string to collect the result
+	set stack {} ;# track open tags
+	set rest {}
+	foreach item "{$xml}" {
+	 	switch -regexp -- $item {
+			^# {append res "{[lrange $item 0 end]} " ; #text item}
+			^/ {
+				regexp {/(.+)} $item -> tagname ;# end tag
 				set tagname [string trim $tagname]
-                set expected [lindex $stack end]
-                if {$tagname!=$expected} {error "$item != $expected"}
-                set stack [lrange $stack 0 end-1]
-                append res "\}\} "
-          }
-            /$ { # singleton - start and end in one <> group
-               regexp {([^ ]+)( (.+))?/$} $item -> tagname - rest
-               set rest [lrange [string map {= " "} $rest] 0 end]
-               append res "{$tagname [list $rest] {}} "
-            }
-            default {
-               set tagname [lindex $item 0] ;# start tag
-               set rest [lrange [string map {= " "} $item] 1 end]
-               lappend stack $tagname
-               append res "\{$tagname [list $rest] \{"
-            }
-         }
-         if {[llength $rest]%2} {error "att's not paired: $rest"}
-     }
-     if {[llength $stack]} {error "unresolved: $stack"}
-     string map {"\} \}" "\}\}"} [lindex $res 0]
+				set expected [lindex $stack end]
+				if {$tagname!=$expected} {error "$item != $expected"}
+				set stack [lrange $stack 0 end-1]
+				append res "\}\} "
+			}
+			/$ { # singleton - start and end in one <> group
+				regexp {([^ ]+)( (.+))?/$} $item -> tagname - rest
+				set rest [lrange [string map {= " "} $rest] 0 end]
+				append res "{$tagname [list $rest] {}} "
+			}
+			default {
+				set tagname [lindex $item 0] ;# start tag
+				set rest [lrange [string map {= " "} $item] 1 end]
+				lappend stack $tagname
+				append res "\{$tagname [list $rest] \{"
+			}
+		}
+		if {[llength $rest] % 2} {error "att's not paired: $rest"}
+	}
+	if {[llength $stack]} {error "unresolved: $stack"}
+	string map {"\} \}" "\}\}"} [lindex $res 0]
 }
 
 # Now that this went so well, I'll throw in the converse:
